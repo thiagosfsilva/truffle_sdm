eae312e57c036a28029095c2a92f4f23d0937f78
Thiago Silva <ts35@stir.ac.uk>
2019-10-03 06:54:39 +0100

    changed CHELSA download script to use Rstudio API for downloading from terminal (use WSL on ruindows).

--- scripts/02_get_chelsa.R
+++ scripts/02_get_chelsa.R
@@ -5,26 +5,33 @@
 library(raster) # For reading and manipulating rasters
 library(sf) # For managing vector data
 library(tmap) # For map visualization
+library(rstudioapi) # For interaction with the WSL terminal
 
 ### Download CHELSA data 
 
 # Using wget on the command line:
-setwd('"D:/Projects/truffle_sdm/env_data/CHELSA/present')
-system('wget -r --no-parent -e robots=off --accept *.tif https://www.wsl.ch/lud/chelsa/data/bioclim/integer/')
+tlist <- terminalList() # Get the terminal ID
 
-setwd('D:/Projects/truffle_sdm/env_data/CHELSA/future/')
-system('wget -r --no-parent -e robots=off --accept *.tif https://www.wsl.ch/lud/chelsa/data/cmip5/2061-2080/bio/')
+terminalSend(tlist[1],'cd env_data/CHELSA/present\n') # Change directory on terminal
+# Send download command to terminal
+terminalSend(tlist[1],
+             'wget -r --no-parent -e robots=off --accept *.tif https://www.wsl.ch/lud/chelsa/data/bioclim/integer/\n')
 
-
+terminalSend(tlist[1],'cd env_data/CHELSA/future/\n')
+terminalSend(tlist[1],
+             'wget -r --no-parent -e robots=off --accept *.tif https://www.wsl.ch/lud/chelsa/data/cmip5/2061-2080/bio/\n')
 
 ### Loading and cutting rasters
 
 # First, we get the vector file with the British Isles
+# bis_sf <- readRDS('carto_data/british_isles.rds')
 
-bis_sf <- readRDS('carto_data/british_isles.rds')
+# Get raster file with mask for Europe
+eu_mask <- raster('carto_data/EUmask.tif')
+plot(eu_mask)
 
 # Then, we read the global CHELSA files from disk as a stack:
-ch_files <- list.files('env_data/CHELSA/www.wsl.ch/lud/chelsa/data/bioclim/integer/',
+ch_files <- list.files('env_data/CHELSA/present/',
                        pattern = 'tif$',
                        full.names = TRUE)
 
